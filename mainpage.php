741fc6022991976be8392ea2729f7f8fe9abf46c
Thomas Wasynczuk <ThomasWasynczuk@wustl.edu>
2019-09-24 21:15:44 -0500

    Update mainpage.php

--- mainpage.php
+++ mainpage.php
@@ -8,6 +8,45 @@
     Computer Science Newz
 </head>
 <body>
-    
+
+<?php    
+require 'database.php';
+// add and view stories -- include usr for delete privs
+// 
+$user = $_POST['user'];
+$title = $_POST['title'];
+$story = $_POST['story'];
+
+$stmt = $mysqli->prepare("insert into stories (user, title, story) values (?, ?, ?)");
+if(!$stmt){
+	printf("Query Prep Failed: %s\n", $mysqli->error);
+	exit;
+}
+
+$stmt->bind_param('sss', $user, $title, $story);
+
+$stmt->execute();
+
+$stmt->bind_result($user, $title, $story);
+
+echo "<ul>\n";
+while($stmt->fetch()){
+	printf("\t<li>%s %s</li>\n",
+		htmlspecialchars($user),
+        htmlspecialchars($title),
+        htmlspecialchars($story)
+	);
+}
+echo "</ul>\n";
+
+$stmt->close();
+
+
+
+
+?>
 </body>
 </html>
+
+<!-- database: allusers
+     Tables: stories and userinfo -->
